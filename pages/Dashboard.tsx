16ea442f896fb0aeecf1f9a9e2595ad2185d3f68
Dev Ruhela <leo18jin@gmail.com>
2025-09-15 23:35:36 +0530

    Update Dashboard.tsx

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -19,7 +19,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 import HabitTracker from '../components/HabitTracker';
 import NotesTaker from '../components/NotesTaker';
 
-// Elegant Shape 
+// Elegant 
 const ElegantShape = ({
   className,
   delay = 0,
